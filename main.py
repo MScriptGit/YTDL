c9bade4b3dfb74858c4afd5e94d53b7c077841f6
MScriptGit <marcelcolt@gmail.com>
2025-04-17 15:06:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 #Made by MScript 2025
+from pytubefix import Search
+
+print
+
+def YTSearch(userInput):
+    userInput = input("Enter Search terms: ")
+  
+    listTitle = []
+    listDuration = []
+    listURL = []
+
+    results = Search(userInput)
+        
+    for video in results.videos:
+        listTitle.append(f'{video.title}')
+        listDuration.append(f'Duration: {video.length} sec')
+        listURL.append(f'URL: {video.watch_url}')
+        
+    return listTitle, listDuration, listURL
+
